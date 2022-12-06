8a9d46eb127cf6809d49c348ebf7b80924eed9ef
FlakoGuN <zamjoseram@gmail.com>
2022-12-06 11:04:43 -0300

    se actualizo el curso
    dandole con toodoo

--- myapp/urls.py
+++ myapp/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('about/', views.about),
     path('projects/', views.projects),
     path('tasks/', views.tasks),
+    path('create_task/', views.create_task),
+    path('create_project/', views.create_project),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse
 from .models import Project, Task
-
+from .forms import CreateNewTask, CreateNewProject
 # Create your views here.
 
 def hello(request):
@@ -17,3 +17,18 @@ def projects(request):
 def tasks(request):
     tasks = Task.objects.all()
     return render (request, 'tasks.html', {'tasks':tasks})
+
+def create_task(request):
+    if request.method == 'GET':
+        return render(request, 'create_task.html', {'form': CreateNewTask()})
+    else:
+       Task.objects.create(title=request.POST['title'], description=request.POST['description'], project_id=2)
+       return redirect ('/tasks/')
+
+def create_project(request):
+    if request.method == 'GET':
+        return render (request, 'create_project.html', {'form': CreateNewProject()})
+    else:
+       Project.objects.create(name=request.POST["name"])
+       
+    return redirect ('/projects/')
